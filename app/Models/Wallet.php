5ecc1c049e2270d57bbdc4726c11119955ab17f8
Marrieon <staceymarrion2002@gmail.com>
2026-02-24 19:52:16 +0300

    Enable model factories and remove resource wrapping

--- app/Models/Wallet.php
+++ app/Models/Wallet.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Model;
@@ -9,6 +10,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Wallet extends Model
 {
+    use HasFactory;
     use SoftDeletes;
 
     protected $fillable = [
